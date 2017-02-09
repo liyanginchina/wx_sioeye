8d70ab20956444b63765537548f3ff3658debf17
jack.li <jack.li@ck-telecom.com>
2017-02-09 10:42:17 +0800

    添加“关注”功能模块加载框架；添加网络请求函数封装

--- page/discovery/discovery.js
+++ page/discovery/discovery.js
@@ -3,6 +3,8 @@ Page({
     text: "Page discovery",
 
     background: ['green', 'red', 'yellow'],
+    image:["https://sioeye-2-0-resource.s3.cn-north-1.amazonaws.com.cn/ad/14846976/fc30d673c77d4bb6927b315978348d77.jpg","https://sioeye-2-0-resource.s3.cn-north-1.amazonaws.com.cn/ad/14840928/11ed6f70d14c430498db549c5232a25c.jpg","https://sioeye-2-0-resource.s3.cn-north-1.amazonaws.com.cn/ad/14837472/5df4274251b04ae9b4d6f3e2abf0139a.jpg",
+    "https://sioeye-2-0-resource.s3.cn-north-1.amazonaws.com.cn/ad/14828832/d9c95586f6e6405e8db163004cf51a0b.jpg","https://sioeye-2-0-resource.s3.cn-north-1.amazonaws.com.cn/ad/14847840/725d8c2e3bc84a99af9fe6c634161776.jpg"],
     indicatorDots: true,
     vertical: false,
     autoplay: true,
--- page/follow/follow.js
+++ page/follow/follow.js
@@ -1,20 +1,72 @@
+var network_util = require('../../utils/network_util.js');
+
 Page({
-  data:{
-    text:"Page follow"
+  data: {
+    list: [],
+    dd: '',
+    hidden: false,
+    page: 1,
+    size: 20,
+    hasMore: true,
+    hasRefesh: false
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    this.loadMore('');    
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+
+  //事件处理函数
+  bindViewTap: function (e) {
+    console.log(e.currentTarget.dataset.title);
+  },
+
+  loadMore: function (e) {
+    var that = this;
+    that.setData({
+      hasRefesh: false,
+    });
+    if (!this.data.hasMore) return
+    var url = 'http://v.juhe.cn/weixin/query?key=f16af393a63364b729fd81ed9fdd4b7d&pno=' + (++that.data.page) + '&ps=10';
+    network_util._get(url,
+      function (res) {
+        that.setData({
+          list: that.data.list.concat(res.data.result.list),
+          hidden: true,
+          hasRefesh: false,
+        });
+      }, function (res) {
+        console.log(res);
+      })
+  },
+
+  refesh: function (e) {
+    var that = this;
+    that.setData({
+      hasRefesh: true,
+    });
+    var url = 'http://v.juhe.cn/weixin/query?key=f16af393a63364b729fd81ed9fdd4b7d&pno=1&ps=10';
+    network_util._get(url,
+      function (res) {
+        that.setData({
+          list: res.data.result.list,
+          hidden: true,
+          page: 1,
+          hasRefesh: false,
+        });
+      }, function (res) {
+        console.log(res);
+      })
   }
 })
